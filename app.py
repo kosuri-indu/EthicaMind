a926b700b7d4ac6962063d339b41e0595c627cdf
Kosuri Indu <kosurilindu@gmail.com>
2025-03-01 10:39:26 +0530

    added host

--- app.py
+++ app.py
@@ -99,4 +99,5 @@ def error():
     return render_template("error.html", error_message=error_message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
